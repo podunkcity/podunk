e277dbbb74324dbd801d302b9dd88814c89ae0db
Rahmat M. Samik-Ibrahim <19358416+rms46@users.noreply.github.com>
2023-07-09 16:38:56 +0700

    modified:   docusaurus.config.js

--- Docusaurus/docusaurus.config.js
+++ Docusaurus/docusaurus.config.js
@@ -15,24 +15,6 @@ const config = {
   projectName: 'Podunk',          // (Usually your repo name)
   trailingSlash: true,              // "true/" or "false"
 
-
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
